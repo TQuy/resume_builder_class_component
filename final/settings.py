3def1c85476e89b6366bf1355dd7376503ec6391
quynt <quynguyenthe000@gmail.com>
2022-05-18 23:45:07 +0700

    downgrade django

--- final/settings.py
+++ final/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "resume-builder-env.eba-z2m4fpyd.ap-southeast-1.elasticbeanstalk.com",
-    "127.0.0.1"
+    "127.0.0.1",
+    "localhost"
     ]
 
 
@@ -83,7 +84,7 @@ WSGI_APPLICATION = 'final.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
